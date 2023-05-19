95a16028d3c48eb6a8cb8d41fb386b922ee4b6ba
David C <dchung20315@gmail.com>
2023-05-18 22:28:47 -0700

    created models and schemas for duelists, thoughts, and reactions

---
+++ models/Duelist.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const duelistSchema = new mongoose.Schema({
+    duelistname: {type: String, required: true, unique: true, trim: true  },
+    email: {type: String, required: true, unique: true, validate: function(email) {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email) ? true : "Invalid Email Address";
+    }},
+    thoughts: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Thought"
+    },
+    rivals: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Duelist"
+    }
+});
+
+duelistSchema.virtual('rivalCount').get(function() {
+    return this.rivals.length;
+});
+
+const Duelist = mongoose.model('Duelist', duelistSchema);
+
+const handleError = (err) => console.error(err);
+
+Duelist.create({
+    duelisttname: 'Yugi Muto',
+    email: 'poweroffriendship@gmail.com',
+    thoughts: ['Dark Magician Girl is so cute!'],
+    rivals: [{
+        duelistname: 'Seto Kaiba'
+    },
+    {
+        duelistname: 'Joey Wheeler'
+    }]
+} , (err) => (err ? handleError(err) : console.log('Duelist created!'))
+);
+
+Duelist.create({
+    duelisttname: 'Seto Kaiba',
+    email: 'screwtherules+ihavemoney@gmail.com',
+    thoughts: ['Blue Eyes White Dragon is the best card ever!'],
+    rivals: [{
+        duelistname: 'Yugi Muto'
+    },
+    {
+        duelistname: 'Joey Wheeler'
+    }]
+} , (err) => (err ? handleError(err) : console.log('Duelist created!'))
+);
+
+Duelist.create({
+    duelistname: 'Joey Wheeler',
+    email: 'timeroulettego@gmail.com',
+    thoughts: ['Red-eyed Black Dragon is the strongest monster!'],
+    rivals: [{
+        duelistname: 'Yugi Muto'
+    },
+    {
+        duelistname: 'Seto Kaiba'
+    }]
+} , (err) => (err ? handleError(err) : console.log('Duelist created!'))
+);
+
---
+++ models/Thought.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {type: String, required: true, minlength: 1, maxlength: 280},
+    createdAt: {type: Date, default: Date.now, get: timestamp => dateFormat(timestamp)},
+    duelistname: {type: String, required: true},
+    reactions: {
+        type: [reactionSchema]
+    }
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
